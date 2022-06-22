7a892bd8e549d53c660e412bde6c7090b1d5025e
Henrik Hautakoski <henrik@eossweden.org>
2022-06-22 11:12:36 +0200

    include/deb_filename.sh: modified to not use regexp. instead take info from arguments

--- include/deb_filename.sh
+++ include/deb_filename.sh
@@ -4,12 +4,18 @@
 #
 #  Correct .deb filenames
 
-# arg1: filename
-# arg2: distro (optional)
-# arg3: version (optional)
-function deb_filename() {
-    local distro=${2:-$(lsb_release -is | tr '[:upper:]' '[:lower:]')}
-	local version=${3:-$(lsb_release -rs)}
+# arg1: package name
+# arg2: package version
+# arg3: package architecture
+# arg4: distro (optional)
+# arg5: distro version (optional)
 
-	echo $1 | sed -E "s/(-${distro})?(-${version})?_([a-z0-9]+)\.deb\$/-${distro}-${version}_\3.deb/"
+function deb_filename() {
+    local name=$1
+    local pkg_ver=$2
+    local pkg_arch=$3
+    local distro=${4:-$(lsb_release -is | tr '[:upper:]' '[:lower:]')}
+	local version=${5:-$(lsb_release -rs)}
+
+    echo "${name}_${pkg_ver}-${distro}-${version}_${pkg_arch}.deb"
 }
