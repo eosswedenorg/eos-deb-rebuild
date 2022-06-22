199968c5a3f90ff8ae14cdc7091df0218b2b67ae
Henrik Hautakoski <henrik@eossweden.org>
2022-06-22 11:26:43 +0200

    Update copyright year.

--- include/plugins/cdt-mv.sh
+++ include/plugins/cdt-mv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright (C) 2019-2020 Henrik Hautakoski <henrik@eossweden.org>. All Rights Reserved.
+#  Copyright (C) 2019-2022 Henrik Hautakoski <henrik@eossweden.org>. All Rights Reserved.
 #
 #  CDT Multiversion specific commands
 
